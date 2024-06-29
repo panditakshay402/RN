a9b6937ee01769e6296bfe6afc8940423aa9a0d6
Akshay pandit <panditakshay402@gmail.com>
2024-06-29 21:30:44 +0530

    minimal code for FlatList

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+
+const DATA=[
+  {
+    id:'1',
+    name:'akshay'
+  },
+  {
+    id:'2',
+    name:'selmon'
+  },
+  {
+    id:'3',
+    name:'aliya'
+  },
+  {
+    id:'4',
+    name:'katrina'
+  },
+  
+]
+
+const Item=({itemPARAM})=>(
+    <View style={{backgroundColor:'blue',margin:10}}>
+    <Text style={{backgroundColor:'red',fontSize:29}}>{itemPARAM}</Text>
+    </View>
+
+
+);
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Image 1</Text>
-      <Image
-        style={styles.image}
-        source={{ uri: 'https://reactnative.dev/img/header_logo.svg' }}
-      />
-      <Text>Image 2</Text>
-      <Image
-        style={styles.image}
-        source={require('./assets/img/logo1.png') }
-      />
-     
+      <Text> FlatList</Text>
+     <FlatList
+     data={DATA}
+     renderItem={({item}) => <Item itemPARAM={item.name} />}
+     keyExtractor={item => item.id}
+     />
     </View>
   );
 };
